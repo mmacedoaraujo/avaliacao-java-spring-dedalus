2152a0ed4aea12a3fb90366a78817b03a2ec1670
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-04-04 19:18:38 -0300

    initial commit

---
+++ src/main/java/com/mmacedoaraujo/avaliacaojavaspringdedalus/AvaliacaoJavaSpringDedalusApplication.java
@@ -0,0 +1,13 @@
+package com.mmacedoaraujo.avaliacaojavaspringdedalus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoJavaSpringDedalusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoJavaSpringDedalusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmacedoaraujo/avaliacaojavaspringdedalus/AvaliacaoJavaSpringDedalusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmacedoaraujo.avaliacaojavaspringdedalus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoJavaSpringDedalusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
