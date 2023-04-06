dbe9b4c89c81002b6ea5a440df7894bd48ba561e
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-04-06 10:24:53 -0300

    succesfully logging exceptions at service layer

--- src/main/java/com/mmacedoaraujo/avaliacaojavaspringdedalus/aspect/LoggingAspect.java
+++ src/main/java/com/mmacedoaraujo/avaliacaojavaspringdedalus/aspect/LoggingAspect.java
@@ -1,12 +1,9 @@
-package com.mmacedoaraujo.avaliacaojavaspringdedalus.logging;
+package com.mmacedoaraujo.avaliacaojavaspringdedalus.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.hibernate.mapping.Join;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -18,12 +15,18 @@ import java.util.Arrays;
 @Slf4j
 public class LoggingAspect {
 
+
     @Pointcut("execution(* com.mmacedoaraujo.avaliacaojavaspringdedalus.controller.UserController..*(..))")
-    protected void loggingOperation() {
+    protected void loggingOperationController() {
 
     }
 
-    @Before("loggingOperation()")
+    @Pointcut("execution(* com.mmacedoaraujo.avaliacaojavaspringdedalus.service.serviceimpl.UserServiceImpl..*(..))")
+    protected void loggingOperationService() {
+
+    }
+
+    @Before("loggingOperationController()")
     @Order(1)
     public void logger(JoinPoint joinPoint) {
         log.info("Signature declaring type : " + joinPoint.getSignature().getDeclaringTypeName());
@@ -32,10 +35,17 @@ public class LoggingAspect {
         log.info("Target class : " + joinPoint.getTarget().getClass().getName());
     }
 
-    @AfterReturning(pointcut = "loggingOperation()", returning = "result")
+    @AfterReturning(pointcut = "loggingOperationController()", returning = "result")
     @Order(2)
     public void logAfter(JoinPoint joinPoint, Object result) {
         log.info("Exiting from method: " + joinPoint.getSignature().getName());
         log.info("Return value: " + result);
+    }
+
+    @AfterThrowing(value = "loggingOperationService()", throwing = "e")
+    @Order(3)
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        log.error("An exception has been thrown in " + joinPoint.getSignature().getName() + "()");
+        log.error("Cause " + e.getCause());
     }
 }
