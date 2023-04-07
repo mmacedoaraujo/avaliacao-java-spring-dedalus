df6d304b955e140195d6183f5fc80c6547ef0b1e
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-04-07 10:20:21 -0300

    configured more routes on gateway

--- gateway/src/main/java/com/mmacedoaraujo/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/com/mmacedoaraujo/gateway/config/GatewayConfig.java
@@ -8,16 +8,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
+    private static final String USERS_API_URI = "http://localhost:8081";
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("getAllUsers", route -> route.path("/users")
-                        .filters(filter -> filter.rewritePath("users", "/api/v1/users"))
-                        .uri("http://localhost:8081")
+                .route("returnUsersRegistered", route -> route.path("/users")
+                        .filters(filter -> filter.rewritePath("/users", "/api/v1/users"))
+                        .uri(USERS_API_URI)
+                )
+                .route("returnUsersRegisteredPageable", route -> route.path("/usersPaginated")
+                        .filters(filter -> filter.rewritePath("/usersPaginated", "/api/v1/users/paginated"))
+                        .uri(USERS_API_URI)
+                )
+                .route("returnUserById", route -> route.path("/users/**")
+                        .filters(filter -> filter.rewritePath("/users/(?<id>.*)", "/api/v1/users/${id}"))
+                        .uri(USERS_API_URI)
                 )
                 .route("addNewUser", route -> route.path("/addNewUser")
-                        .filters(filter -> filter.rewritePath("addNewUser", "/api/v1/users/addNewUser"))
-                        .uri("http://localhost:8081")
+                        .filters(filter -> filter.rewritePath("/addNewUser", "/api/v1/users/addNewUser"))
+                        .uri(USERS_API_URI)
+                )
+                .route("updateUserData", route -> route.path("/updateUser/**")
+                        .filters(filter -> filter.rewritePath("/updateUser/(?<id>.*)", "/api/v1/users/updateUser/${id}"))
+                        .uri(USERS_API_URI)
                 )
                 .build();
     }
